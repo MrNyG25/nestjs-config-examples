3e9515c9e5e5cbaaea08f81b6d009a4d1173a590
Neider García <yesid19999@gmail.com>
2022-05-14 18:01:37 -0500

    log message and whitelist for DTO properties

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 ```
 npm i --save @nestjs/config
 ```
+
+### to use ValidationPipe and validate DTOs too
+```
+npm i class-transformer
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  //validation for models
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /*to delete not desired properties from dtos (sended by the client)*/
+      whitelist: true, 
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+   //log host and port
+   const logger = new Logger();
+   logger.log(`Server on port: ${await app.getUrl()} `);
 }
 bootstrap();
